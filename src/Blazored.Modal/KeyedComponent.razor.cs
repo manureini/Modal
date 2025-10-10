17f07c5b6d30ab8790e942ba895bb5f8ba30e06a
Felipe Costa Gualberto <felipebenza@hotmail.com>
2025-10-10 20:43:48 -0300

    Fixes #586.

---
+++ src/Blazored.Modal/KeyedComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Blazored.Modal;
+
+internal class KeyedComponent : ComponentBase
+{
+    [Parameter] public RenderFragment ChildContent { get; set; } = default!;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        ChildContent(builder);
+    }
+}
